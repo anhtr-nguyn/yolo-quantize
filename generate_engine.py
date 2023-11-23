2637d2fe5d1038ed003a287ecc3bc923452665cf
anhtr-nguyn <anhnguyen.0912a@gmail.com>
2023-11-23 15:47:28 +0700

    generate tensorRT engine

--- generate_engine.py
+++ generate_engine.py
@@ -148,14 +148,63 @@ def run(
     output = {}
 
     print("include: {}".format(include))
-    # if 'engine' in include:
-        # output['engine'], _ = export_engine(model, im, file, half, dynamic, simplify, workspace, verbose)
+    if 'engine' in include:
+        output['engine'], _ = export_engine(model, im, file, half, dynamic, simplify, workspace, verbose)
     if 'onnx' in include:
         output['onnx'], _ = export_onnx(model, im, file, opset, dynamic, simplify)
 
     duration = time.time() - t
     LOGGER.info(f'\nExport complete ({duration:.1f}s)')
 
+@try_export
+def export_engine(model, im, file, half, dynamic, simplify, workspace=4, verbose=False, prefix=colorstr('TensorRT:')):
+
+    #check device
+    assert im.device.type != 'cpu', 'export running on CPU but must be on GPU, i.e. `python export.py --device 0`'
+    try:
+        import tensorrt as trt
+    except Exception as e:
+        print(e)
+
+    check_version(trt.__version__, '8.0.0', hard=True)  # require tensorrt>=8.0.0
+    export_onnx(model, im, file, 12, dynamic, simplify)  # opset 12
+    onnx = file.with_suffix('.onnx')
+    LOGGER.info(f'\n{prefix} starting export with TensorRT {trt.__version__}...')
+    assert onnx.exists(), f'failed to export ONNX file: {onnx}'
+    f = file.with_suffix('.engine')  # TensorRT engine file
+    logger = trt.Logger(trt.Logger.INFO)
+    if verbose:
+        logger.min_severity = trt.Logger.Severity.VERBOSE
+
+    builder = trt.Builder(logger)
+
+    config = builder.create_builder_config()
+    config.max_workspace_size = workspace * 1 << 30
+
+    flag = (1 << int(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
+    network = builder.create_network(flag)
+
+    parser = trt.OnnxParser(network, logger)
+    if not parser.parse_from_file(str(onnx)):
+        raise RuntimeError(f'failed to load ONNX file: {onnx}')
+    
+    inputs = [network.get_input(i) for i in range(network.num_inputs)]
+    outputs = [network.get_output(i) for i in range(network.num_outputs)]
+
+    if dynamic:
+        if im.shape[0] <= 1:
+            LOGGER.warning(f'{prefix} WARNING ⚠️ --dynamic model requires maximum --batch-size argument')
+        profile = builder.create_optimization_profile()
+        for inp in inputs:
+            profile.set_shape(inp.name, (1, *im.shape[1:]), (max(1, im.shape[0] // 2), *im.shape[1:]), im.shape)
+        config.add_optimization_profile(profile)
+
+    LOGGER.info(f'{prefix} building FP{16 if builder.platform_has_fast_fp16 and half else 32} engine as {f}')
+    if builder.platform_has_fast_fp16 and half:
+        config.set_flag(trt.BuilderFlag.FP16)
+    with builder.build_engine(network, config) as engine, open(f, 'wb') as t:
+        t.write(engine.serialize())
+    return f, None
 
 
 @try_export
@@ -213,6 +262,8 @@ def export_onnx(model, im, file, opset, dynamic, simplify, prefix=colorstr('ONNX
         except Exception as e:
             LOGGER.info(f'{prefix} simplifier failure: {e}')
     return onnx_file_name, model_onnx
+
+
 def main(opt):
     run(**vars(opt))
 
